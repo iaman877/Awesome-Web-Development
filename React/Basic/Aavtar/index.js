a0a499f4d22a078b17fbb3a276982c41472a32a7
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-02-09 13:04:54 +0530

    intial commit

---
+++ React/Basic/Aavtar/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Avtar from './Avtar'
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(<Avtar />, document.getElementById('root'));
+reportWebVitals();
