c75388425b07eacdb820b6d693cc7edefed21847
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-02-09 20:54:47 +0530

    Initial Commit

---
+++ React/Basic/src/Avtar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Avtar.css';
+const Avtar = () => {
+    return <div className="avtarstyle"> 
+              <img src="https://joeschmoe.io/api/v1/aman" alt = "Avtar" />
+              <h1> Aman Bhardwaj</h1>
+              <p> Web Developer</p>
+            </div>
+}
+export default Avtar;
