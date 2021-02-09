befbb1831457ec50dfdb948a776a045c06ff1448
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-02-09 20:56:50 +0530

    Delete index.js

--- React/Basic/Avatar/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Avatar from './Avatar'
-import reportWebVitals from './reportWebVitals';
-ReactDOM.render(<Avtar />, document.getElementById('root'));
-reportWebVitals();
