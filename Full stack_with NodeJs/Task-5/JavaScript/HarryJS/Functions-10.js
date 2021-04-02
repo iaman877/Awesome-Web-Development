02f684ace44fe0c864fbb03c726b199bcf748c96
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-04-02 22:10:28 +0530

    created

---
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Functions-10.js
@@ -0,0 +1,14 @@
+      if(1){
+          let i =234;
+          console.log(i)
+      }
+      console.log(i);
+
+      function ui(name)
+      {
+          let i = 9;
+          console.log(i);
+          return `This is a ${name} ui`;
+      }
+
+console.log(ui("harry"), i)
