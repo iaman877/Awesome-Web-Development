7965961dde79e9d20843aa128bcf589d74454498
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-04-02 17:07:03 +0530

    Initial Commit

---
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Template Literals-6.js
@@ -0,0 +1,7 @@
+let simple = `This is an example of
+                     Template literal`;
+
+          let firstName = 'Linux',
+          lastName = 'Love';         
+          let greeting = `Welcome to ${firstName} ${lastName}`;
+          console.log(greeting); //Welcome to Linux Love
