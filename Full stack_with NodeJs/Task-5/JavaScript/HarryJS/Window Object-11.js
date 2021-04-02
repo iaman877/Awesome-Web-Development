ca20f91054f0df3d719bf067e1fcf9728535bbf3
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-04-02 23:38:53 +0530

    Created

---
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Window Object-11.js
@@ -0,0 +1,8 @@
+// The browser automatically creates a window object. The window is not the object of the JavaScript.
+let a = window.document;
+a = window.innerHeight;
+a = innerWidth;
+a = scrollY;
+a = location.toString();
+a = window.history;
+console.log(a);     
