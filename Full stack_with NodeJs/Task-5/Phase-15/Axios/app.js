7d63ac19470a8b74310866df693eeea665e47a98
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-04-18 19:40:07 +0530

    Create app.js

---
+++ Full stack_with NodeJs/Task-5/Phase-15/Axios/app.js
@@ -0,0 +1,8 @@
+
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(fetchedData => {
+        console.log(fetchedData.data.ticker.price);
+    })
+    .catch(err => {
+        console.log(err);
+    })
