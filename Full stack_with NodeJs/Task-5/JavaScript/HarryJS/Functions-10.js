435a133773ed0054fe5d74e6be6ffa3766367c91
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-18 18:48:46 +0530

    Update Functions-10.js

--- Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Functions-10.js
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Functions-10.js
@@ -12,3 +12,30 @@
       }
 
 console.log(ui("harry"), i)
+// ---------------------------  Another Way to use function --------------------------
+var getUserRole = function (name, role) {
+  switch (role) {
+    case "admin":
+      return `${name} is admin with all access`;
+      break; // this is not necessary
+    case "subadmin":
+      return `${name} is sub-admin with access to create and delete courses`;
+      break;
+    case "testprep":
+      return `${name} is a test prep with access to create and delete tests`;
+      break;
+    case "user":
+      return `${name} is a user to consume content`;
+      break;
+
+    default:
+      return `${name} is a trial user`;
+      break;
+  }
+};
+
+console.log(getUserRole("hitesh", "testprep"));
+var getRole = getUserRole("sammy", "user");
+console.log(getRole);
+
+
