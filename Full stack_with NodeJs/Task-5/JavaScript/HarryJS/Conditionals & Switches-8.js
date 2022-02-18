25d4b182cd4222dff1381c4921a9eee0b97bff46
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-18 16:59:48 +0530

    Update Conditionals & Switches-8.js

--- Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Conditionals & Switches-8.js
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Conditionals & Switches-8.js
@@ -44,3 +44,31 @@ switch (age) {
         console.log("You are unknown age");
         break;
 }   
+// -----------------------------------------------  If -----------------------------------
+var email = true;
+var facebook = false;
+var google = true;
+
+if (email || facebook || google) {
+  console.log("Login Success");
+}
+// --------------------------------------------- Switch ----------------------------------
+var user = "admin";
+
+switch (user) {
+  case "admin":
+    console.log("You get full access");
+    break;
+  case "subadmin":
+    console.log("gets access to create/delete courses");
+    break;
+  case "testprep":
+    console.log("gets access to create/delete tests");
+    break;
+  case "user":
+    console.log("gets access to consume content");
+    break;
+  default:
+    console.log("Trial user");
+    break;
+}
