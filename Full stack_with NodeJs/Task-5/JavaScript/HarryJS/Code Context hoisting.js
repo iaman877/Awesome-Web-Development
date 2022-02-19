b79d4e724ab382c2e81d9eddfb3e13544fea9320
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-19 11:48:05 +0530

    Initial commit

---
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Code Context hoisting.js
@@ -0,0 +1,49 @@
+/*
+Execution context
+ 1. Variable Object 
+ 2. Scope Chain
+ 3. this
+ */
+// Below code will not give any error, function declaration are scanned and made available 
+tipper("80");
+function tipper(a) {
+  var bill = parseInt(a);
+  console.log(bill + 5);
+}
+// Below code will not give any error, function declaration are scanned and made Undefined
+bigTipper("200");
+var bigTipper = function (a) {
+  var bill = parseInt(a);
+  console.log(bill + 15);
+};
+// or 
+console.log(name);
+var name = "hitesh";
+// ----------------------------------- Scope chaining --------------------------------
+var name = "AMan";
+
+console.log("Line number 25", name);
+
+function sayName() {
+  var name = "Mr. H";
+  console.log("Line number 29", name);
+  sayNameTwo();
+
+  function sayNameTwo() {
+  //  var name = "Mr. HC";
+    console.log("Line number 34", name);
+  }
+}
+sayName();
+
+//--------- this keyword
+console.log(this);
+
+var game = "basketball";
+
+// function sayName() {
+//   var name = "Hitesh";
+//   console.log(this);
+// }
+
+// sayName();
