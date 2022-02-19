f94c3f5424b7ef07dd6b4eafff40f6f0517e828f
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-19 17:01:04 +0530

    Update app.js

--- Full stack_with NodeJs/Task-5/JavaScript/phase-9/Arrays/app.js
+++ Full stack_with NodeJs/Task-5/JavaScript/phase-9/Arrays/app.js
@@ -1,34 +1,24 @@
 //Basic Array Methods
 
 let fruits = ["Apple", "Guava", "Papaya", "Litchi"];
-// console.log(fruits);
 
-
-// push--------------------------------------
+// push
 
 // fruits.push('Grapes')
-fruits.push('Grapes', 'Mango');
-
-// console.log(fruits);
+fruits.push('Grapes', 'Mango');   // console.log(fruits);
 
 // pop-----------------------------------------
 
-let popedFruit = fruits.pop();
-// console.log(popedFruit);
-// console.log(fruits);
+let popedFruit = fruits.pop();   // console.log(popedFruit);  console.log(fruits);
 
 // shift-----------------------------------------
 
-let shiftedFruit = fruits.shift();
-// console.log(shiftedFruit);
-// console.log(fruits);
+let shiftedFruit = fruits.shift();  // console.log(shiftedFruit); console.log(fruits);
 
 // Unshift------------------------------------
 
 // fruits.unshift('Strawberry')
-fruits.unshift('Strawberry','Pineapple')
-
-// console.log(fruits);
+fruits.unshift('Strawberry','Pineapple');  // console.log(fruits);
 
 
 // More array methods
@@ -42,13 +32,29 @@ let arr2 = ['blue', 'green', 'orange'];
 let arr3 = arr2.concat(arr1);
 console.log(arr3);
 
-// includes--------------------------
+// array & call back function 
 
-let colors = ['red', 'green', 'pink', 'purple'];
+var isEven = (element) => 
+{
+  return element % 2 === 0;
+};                          
 
+/*
+var result = [2, 3, 6, 8].every(isEven);
+console.log(result);
+*/
 
-// reverse------------------------
+var result = [2, 3, 6, 8].every((e) => (e % 2 === 0));
+console.log(result);
 
+// ------------------------- splice() and Filter ------
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8]; //console.log(testArray.fill("empty"));
+const myNumber = [23, 24, 25, 55, 66, 77, 87, 98];
+const result = myNumber.filter((num) => num > 55);  //console.log(result);
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
 
-
+// console.log(users.slice(1));
+users.splice(1, 3, "HI");
+users.splice(1, 3, "HI", "BYE");
+users.splice(); // console.log(users);
 
