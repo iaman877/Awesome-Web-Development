1a8f2990c7ae4c79e29b9a786b362a822665aa97
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-20 14:22:17 +0530

    Update Code Context hoisting.js

--- Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Code Context hoisting.js
+++ Full stack_with NodeJs/Task-5/JavaScript/HarryJS/Code Context hoisting.js
@@ -47,3 +47,21 @@ var game = "basketball";
 // }
 
 // sayName();
+//----------------------------------------
+
+console.log(this);
+
+var user = {
+  firstName: "Hitesh",
+  courseCount: 4,
+  getCourseCount: function () {
+    console.log("LINE 7", this);
+    function sayHello() {
+      console.log("Hello");
+      console.log("LINE 10", this);
+    }
+    sayHello();
+  },
+};
+
+user.getCourseCount();
