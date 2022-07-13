cc4483e5456aa975f1909f50709ace3a60fe458f
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-07-13 09:56:22 +0530

    added definition

--- JavaScript/arrays/2-includes.js
+++ JavaScript/arrays/2-includes.js
@@ -1,4 +1,12 @@
 
+/*
+
+In JavaScript, includes() method determines whether a string contains the given characters within it or not. This method returns true if the string contains the characters, otherwise, it returns false. 
+Note: The includes() method is case sensitive i.e, it will treat the Uppercase characters and Lowercase characters differently.
+Syntax:  string.includes(searchvalue, start)
+
+*/
+
 const numbers = [1, 2, 3, 4]; 
 
 console.log(includes(numbers, -1));
